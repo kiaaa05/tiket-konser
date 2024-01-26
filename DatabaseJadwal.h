1e8acd1f51d0b65b78536ab2f552ff72b1835874
kiaaa05 <157813946+kiaaa05@users.noreply.github.com>
2024-01-26 21:10:02 +0700

    Add files via upload

---
+++ DatabaseJadwal.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string kodeKonser [100];
+string namaKonser [100];
+string tanggalKonser [100];
+string tempatKonser[100];
+string jumlahTiketKonser [100];
+string hargaKonser [100];
+int totalKonser =0;
---
+++ DatabaseUser.h
@@ -0,0 +1,8 @@
+#include <string>
+
+using namespace std;
+
+string usernameUser[100];
+string passwordUser[100];
+string emailUser[100];
+int totalUser = 0;
